78c643ee81503fbc7b46a0758f6b526dab4a4b5d
ReginaRay <miss.rayanova@gmail.com>
2017-11-02 17:55:18 +0300

    commit

---
+++ homework7/generators.py
@@ -0,0 +1,25 @@
+import random
+from datetime import datetime, timedelta
+
+
+def all_even_numbers():
+    for i in range(0,100,2):
+        yield i
+
+
+def random_increasing_number(start_from = 0):
+    last_num = start_from
+    next_num = random.randint(last_num, last_num + 100)
+    while True:
+        next_num = random.randint(next_num, next_num+100)
+        yield next_num
+
+
+def next_day():
+    i = 0
+    today = datetime.datetime.today().date()
+
+    while True:
+        yield today
+        today = today + datetime.timedelta(days=i)
+        i += 1
